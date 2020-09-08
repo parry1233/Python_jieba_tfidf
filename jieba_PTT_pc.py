ad01860f9d5cee5f443c9a375c40305713ae9937
Perry <49940223+parry1233@users.noreply.github.com>
2020-09-08 20:47:15 +0800

    WordCloud
    add word cloud

--- jieba_PTT_pc.py
+++ jieba_PTT_pc.py
@@ -1,6 +1,11 @@
 import requests
 import pandas as pd
 import jieba.analyse
+import numpy as np
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud
+from PIL import Image
 from bs4 import BeautifulSoup
 
 req = requests.session()
@@ -9,6 +14,20 @@ pc_soup = BeautifulSoup(pc_list.text)
 file_temp=[]
 title_name = pc_soup('title')[0].text
 print(title_name)
+words = []
+
 for i in pc_soup.select('.push-content'):
-    imp = jieba.analyse.textrank(i.text[1:],topK=3,withWeight=True)
+    imp = jieba.analyse.extract_tags(i.text[1:],topK=3)
     print(i.text[1:],imp)
+    for keyword in imp:
+        words.append(keyword)
+
+words_collect = " ".join(words)
+wordCloudFileName = title_name+'_wordCloud.png'
+img_mask = np.array(Image.open('nvidia.png'))
+wordCloudResult = WordCloud(background_color='white',mask=img_mask,font_path='SoukouMincho.ttf').generate(words_collect)
+plt.figure(figsize=(20,20))
+plt.imshow(wordCloudResult)
+plt.axis("off")
+plt.show()
+wordCloudResult.to_file(wordCloudFileName)
